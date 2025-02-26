ccb4d51be6ab84eb5f808a98d8ae1da5abc6b327
MUKILTEO\1722902 <1722902@mukilteo.wednet.edu>
2025-02-26 12:41:47 -0800

    got rid of some stuff fixed some stuff and made some new stuff

--- 2D Template/Assets/movement.cs
+++ 2D Template/Assets/movement.cs
@@ -1,87 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.InputSystem;
 
-public class movementScript
+[RequireComponent(typeof(Rigidbody2D))]  // this script can only be added to a gameobject with a rigidbody2d
+
+public class movement : MonoBehaviour
 {
-    public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S;
-    public float speed = 10, jumpheight = 15, MaxSpeed = 30;
-    public KeyCode jump = KeyCode.W;
+    public float movementSpeed;
+
     private Rigidbody2D _rb;
-    private Animator _animator;
-    private SpriteRenderer _spriteRenderer;
-    [Header("Raycast")]
+    private Vector2 _moveAmount;
 
-    [SerializeField] float dist; //raycast range
-    [SerializeField] float downRange = -0.75f; //offset
-    [SerializeField] float health, maxHealth = 1;
-
-    bool ground;
-
-
-    private void Start()
+    void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        health = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(left))
-        {
-            _rb.AddForce(Vector2.left * speed * Time.deltaTime);
-            _animator.SetBool("Is_moving", true);
-            _spriteRenderer.flipX = true;
-        }
-        else if (Input.GetKey(right))
-        {
-            _rb.AddForce(Vector2.right * speed * Time.deltaTime);
-            _animator.SetBool("Is_moving", true);
-            _spriteRenderer.flipX = false;
-        }
-        else
-        {
-            _animator.SetBool("Is_moving", false);
-        }
-        if (Input.GetKey(up))
-        {
-            _rb.linearVelocity = Vector2.up * speed;
-        }
-        if (Input.GetKeyDown(down))
-        {
-            _rb.linearVelocity = Vector2.down * speed;
-        }
-        if (_rb.linearVelocity.x > MaxSpeed)
-            _rb.linearVelocity = new Vector2(MaxSpeed, _rb.linearVelocity.y);
-        if (_rb.linearVelocity.x > MaxSpeed)
-            _rb.linearVelocity = new Vector2(-MaxSpeed, _rb.linearVelocity.y);
-        _animator.SetFloat("speed", Mathf.Abs(_rb.linearVelocity.x));
-
-
-        //orgin position * in the down dir downRange, direction, range
-        bool ray = Physics2D.Raycast(transform.position + (Vector3.down * downRange), Vector2.down, dist);
-        //bool ray = Physics2D.BoxCast(transform.position *())
-
-        //print(ray);
-
-        if (ray && Input.GetKeyDown(jump))
-        {
-            _rb.AddForce(Vector2.up * jumpheight);
-            _animator.SetTrigger("Jumped");
-            _animator.SetBool("Is_moving", false);
-        }
-        if (Input.GetKeyUp(jump))
-        {
-            _rb.linearVelocity = new(_rb.linearVelocity.x, _rb.linearVelocity.y / 2);
-        }
+        _rb.linearVelocityX = _moveAmount.x;
     }
 
-    private void OnDrawGizmos()
+    public void HandleMovement(InputAction.CallbackContext ctx)
     {
-        Gizmos.DrawLine(transform.position + (Vector3.down * downRange), transform.position + Vector3.down * dist);
+        _moveAmount = ctx.ReadValue<Vector2>();
+    }
+
+    public void HandleJump(InputAction.CallbackContext ctx)
+    {
+        _rb.linearVelocityY = 10;
     }
 }
